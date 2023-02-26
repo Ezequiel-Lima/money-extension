9c62364a185faba0a6a98d0f3049d5b489b257f5
Ezequiel Lima <joao.lima94@fatec.sp.gov.br>
2023-02-26 16:30:59 -0300

    Add project files.

---
+++ Test/MoneyExtensionTests.cs
@@ -0,0 +1,17 @@
+﻿using MoneyExtension;
+
+namespace Test
+{
+    [TestClass]
+    public class MoneyExtensionTests
+    {
+        [TestMethod] 
+        public void ShouldConvertDecimalToInt()
+        {
+            decimal valor = 279.98M;
+            var cents = valor.ToCents();
+
+            Assert.AreEqual(27998, cents);
+        }
+    }
+}
